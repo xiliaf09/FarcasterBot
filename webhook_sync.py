0170fecb90efbe891ac2607205749dfd46c0fea3
xiliaf <failixpoubelle@gmail.com>
2025-09-02 15:12:40 +0200

    Correction de l'utilisation de get_session_local() - Appel correct de get_session_local()() pour créer une session active - Résout l'erreur 'sessionmaker' object has no attribute 'query' - Toutes les fonctions de base de données maintenant fonctionnelles

--- webhook_sync.py
+++ webhook_sync.py
@@ -19,7 +19,7 @@ def sync_neynar_webhook():
             return
         
         # Récupérer tous les FIDs suivis (toutes guilds confondues)
-        db = get_session_local()
+        db = get_session_local()()
         try:
             tracked_accounts = db.query(TrackedAccount.fid).distinct().all()
             all_fids = [account[0] for account in tracked_accounts]
@@ -97,7 +97,7 @@ def sync_neynar_webhook():
 
 def get_webhook_state():
     """Récupérer l'état actuel du webhook"""
-    db = get_session_local()
+    db = get_session_local()()
     try:
         return db.query(WebhookState).filter_by(id="singleton").first()
     finally:
@@ -112,7 +112,7 @@ def cleanup_webhook():
                 get_neynar_client().delete_webhook(webhook_state.webhook_id)
                 logger.info(f"Webhook Neynar {webhook_state.webhook_id} supprimé")
                 
-                db = get_session_local()
+                db = get_session_local()()
                 try:
                     db.delete(webhook_state)
                     db.commit()
@@ -154,7 +154,7 @@ def get_webhook_stats():
     try:
         logger.info("🔧 Tentative de récupération des stats du webhook...")
         
-        db = get_session_local()
+        db = get_session_local()()
         try:
             webhook_state = db.query(WebhookState).filter_by(id="singleton").first()
             
